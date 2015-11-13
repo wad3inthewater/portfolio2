d5b2e1821176a9a6a6527d7b9dd4e00e29e87919
wad3inthewater <wfulle20@gmail.com>
2015-11-12 23:17:10 -0500

    add rails app

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    jshint = require('gulp-jshint');
+
+
+gulp.task('jshint', function() {
+  gulp.src('./public/javascript/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('sass', function(){
+  gulp.src('./public/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+// gulp.task('sass:watch', function () {
+//   gulp.watch('./sass/**/*.scss', ['sass']);
+// });
+
+gulp.task('default',['jshint','sass'],  function() {
+  //watches for sass changes
+  gulp.watch('./public/sass/**/*.scss',['sass']);
+  //watches for js changes
+  gulp.watch('./public/javascript/**/*.js', ['jshint']);
+});
